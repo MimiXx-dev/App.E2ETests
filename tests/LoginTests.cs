f42ead122543c1c4d4fdf5adb70e688865ab6fb0
AzureAD\AmaniMettichi <amani.mettichi-external@agathalife.com>
2024-06-24 16:22:34 +0100

    commented test code

--- tests/LoginTests.cs
+++ tests/LoginTests.cs
@@ -13,24 +13,24 @@ namespace App.E2E.Tests
             _driver = new ChromeDriver();
         }
 
-        [Fact]
-        public void LoginButton_ShouldNavigateToDashboard_WhenCredentialsAreValid()
-        {
-            // Arrange
+        //[Fact]
+        //public void LoginButton_ShouldNavigateToDashboard_WhenCredentialsAreValid()
+        //{
+            //// Arrange
             //_driver.Navigate().GoToUrl("https://yourapp.com/login"); //ElGen Url
 
             //var usernameField = _driver.FindElement(By.Id("username"));
             //var passwordField = _driver.FindElement(By.Id("password"));
             //var loginButton = _driver.FindElement(By.Id("loginButton"));
 
-            // Act
+            //// Act
             //usernameField.SendKeys("validUsername");
             //passwordField.SendKeys("validPassword");
             //loginButton.Click();
 
-            // Assert
+            //// Assert
             //Assert.Contains("dashboard", _driver.Url, StringComparison.OrdinalIgnoreCase);
-        }
+        //}
 
         public void Dispose()
         {
